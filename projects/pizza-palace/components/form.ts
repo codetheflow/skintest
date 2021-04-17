4baa196e0df1d745737b96b77c65498121eb918a
Dmitry Sitnov <dimitry.sitnov@gmail.com>
2021-04-17 13:04:23 +0300

    pizza palace example

---
+++ projects/pizza-palace/components/form.ts
@@ -0,0 +1,17 @@
+import { $ } from '@testcloud';
+import { env } from '@my/pizza-palace';
+
+export const form = {
+  url: env.url
+
+  , next_step: $<HTMLElement>('.next-step')
+
+  , pie_size_handle: $<HTMLElement>('.noUi-handle')
+  , pizza: (name: string) => $<HTMLElement>(`#${name}`)
+
+  , confirm_addres: $<HTMLButtonElement>('.confirm-address')
+  , phone: $<HTMLInputElement>('#phone-input')
+  , map_zoom_in: $<HTMLButtonElement>('button[title="Zoom in"]')
+
+  , complete_order: $<HTMLButtonElement>('.complete-order')
+};
---
+++ projects/pizza-palace/features/submit-a-form.ts
@@ -0,0 +1,35 @@
+import { feature, I } from '@testcloud';
+import { form } from '@my/pizza-palace/components';
+
+feature('submit a form')
+
+  .before('feature'
+    , I.amOnPage(form.url)
+  )
+
+  .scenario('successfull complete'
+
+    // , I.say('automatically dismiss dialog boxes')
+    // , I.setNativeDialogHandler(() => true)
+
+    , I.say('drag the pizza size slider')
+    , I.drag(form.pie_size_handle, 100, 0)
+
+    , I.say('select the toppings')
+    , I.click(form.next_step)
+    , I.click(form.pizza('pepperoni'))
+    , I.click(form.next_step)
+
+    , I.say('fill the address form')
+    , I.click(form.confirm_addres)
+    , I.fill(form.phone, '+1-541-754-3001')
+    , I.click(form.next_step)
+
+    , I.say('zoom into the iframe map')
+    //, I.switchToIframe('.restaurant-location iframe')
+    , I.click(form.map_zoom_in)
+
+    , I.say('submit the order')
+    //, I.switchToMainWindow()
+    , I.click(form.complete_order)
+  )
