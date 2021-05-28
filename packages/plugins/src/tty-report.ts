849cbb6befea38cde708df46c0cfcb2e3f9727cf
Dmitry Sitnov <dimitry.sitnov@gmail.com>
2021-05-29 02:53:22 +0300

    move write* functions to the tty

--- packages/plugins/src/tty-report.ts
+++ packages/plugins/src/tty-report.ts
@@ -1,28 +1,10 @@
-import { ellipsis, padRight, prettyStack, StringDictionary } from '@skintest/common';
 import { OnStage, Plugin } from '@skintest/platform';
-import { AssertHost, Command, DOMElement, ElementRef, ElementState, InspectInfo, TestFail } from '@skintest/sdk';
-import * as path from 'path';
+import { Command } from '@skintest/sdk';
 import { tty } from './tty';
 
 const { stdout, stderr } = process;
 
 const TAG_RE = /(^|\s)(#[^\s$]+)(\s|$)/gi;
-const KNOWN_ERRORS = new Set(['skintest.timeout']);
-
-const STACK_FUNC_IGNORE = [
-  '__awaiter',
-  'fulfilled',
-  'rejected',
-];
-
-const STACK_FILE_IGNORE = [
-  path.join('platform', 'dist', 'src', 'attempt.js'),
-  path.join('platform', 'src', 'attempt.ts'),
-  path.join('common', 'src', 'errors.ts'),
-  // from playwright
-  // todo: propagate it here for the launcher options
-  path.join('lib', 'utils', 'stackTrace.js')
-];
 
 async function getMessage(command: Command): Promise<string> {
   try {
@@ -30,103 +12,6 @@ async function getMessage(command: Command): Promise<string> {
     return meta.rootage;
   } catch {
     return command.toString();
-  }
-}
-
-async function writeError(reason: Error) {
-  const message = (reason.name || 'Error') + ': ' + (reason.message || 'unknown error');
-  tty.newLine(stderr, tty.error(padRight(message, stderr.columns)));
-
-  if (!KNOWN_ERRORS.has(reason.name) && reason.stack) {
-    const stackTrace = await prettyStack(reason.stack);
-
-    stackTrace
-      .filter(x => x.file)
-      .filter(x => !STACK_FUNC_IGNORE.some(func => x.function === func))
-      .filter(x => !STACK_FILE_IGNORE.some(file => x.file.includes(file)))
-      .map(x => `${x.function || 'at'} (${x.file}:${x.line}:${x.column})`)
-      .forEach(x => tty.newLine(stderr, tty.fail(x)));
-  }
-}
-
-function writeFail(reason: TestFail) {
-  const { body } = reason;
-
-  // todo: make it better
-  const pivot = ['query', 'assert', 'actual', 'etalon'];
-  const isBinaryAssert = pivot.every(key => key in body);
-
-  if (isBinaryAssert) {
-    const selector = body.query.toString();
-    const method = body.query.type === 'query' ? '$' : '$$';
-    const assert: AssertHost = body.assert;
-
-    tty.newLine(stderr, tty.fail(
-      `${method}(${selector}).${assert.what}: ` +
-      `expected ${tty.value('`' + body.actual + '`')} ` +
-      `to ${assert.no ? 'not' : ''} ${assert.how} ` +
-      `${tty.value('`' + body.etalon + '`')}`
-    ));
-  } else {
-    tty.newLine(stderr, tty.fail(reason.description));
-  }
-}
-
-async function writeInspect(inspect: InspectInfo) {
-  // eslint-disable-next-line prefer-const
-  let { selector, target } = inspect;
-  const maxWidth = 40;
-
-  target = Array.isArray(target)
-    ? target.length > 1
-      ? target
-      : target[0]
-    : target;
-
-  if (!target) {
-    tty.newLine(stderr, tty.error(`$(\`${selector}\`) didn't find any elements`));
-    return;
-  }
-
-  if (Array.isArray(target)) {
-    tty.newLine(stdout, `$(\`${selector}\`) found ${target.length} elements`);
-
-    const list: Array<StringDictionary<unknown>> = [];
-    for (const element of target) {
-      list.push({
-        text: ellipsis(await element.text(), maxWidth),
-        visible: await element.state('visible'),
-        enabled: await element.state('enabled'),
-      });
-    }
-
-    tty.newLine(stdout);
-    console.table(list);
-  } else {
-    const elementRef = target as ElementRef<DOMElement>;
-    tty.newLine(stdout, `$(\`${selector}\`) found 1 element`);
-
-    const info: StringDictionary<unknown> = {
-      text: ellipsis(await elementRef.text(), maxWidth),
-      classList: ellipsis((await elementRef.classList()).toString(), maxWidth),
-    };
-
-    const addState = async (key: ElementState) => {
-      try {
-        info[key] = await elementRef.state(key);
-        // eslint-disable-next-line no-empty
-      } catch (ex) {
-      }
-    };
-
-    await addState('checked');
-    await addState('editable');
-    await addState('enabled');
-    await addState('focused');
-    await addState('visible');
-
-    tty.newLine(stdout);
-    console.table(info);
   }
 }
 
@@ -161,7 +46,7 @@ export function ttyReport(): Plugin {
     'step:pass': async ({ site, step, result }) => {
       if (step.type === 'dev') {
         if (result.inspect) {
-          await writeInspect(result.inspect);
+          await tty.writeInspect(stdout, result.inspect);
           return;
         }
       }
@@ -187,18 +72,18 @@ export function ttyReport(): Plugin {
       }
 
       if ('status' in reason) {
-        writeFail(reason);
+        tty.writeFail(stderr, reason);
       } else {
-        await writeError(reason);
+        await tty.writeError(stderr, reason);
       }
     },
     'recipe:fail': async ({ reason, step }) => {
       const message = await getMessage(step);
       tty.replaceLine(stderr, tty.fail(tty.CROSS_MARK), ' ', tty.info(message));
-      await writeError(reason);
+      await tty.writeError(stderr, reason);
     },
     'project:error': async ({ reason }) => {
-      await writeError(reason);
+      await tty.writeError(stderr, reason);
     }
   });
 }
--- packages/plugins/src/tty.ts
+++ packages/plugins/src/tty.ts
@@ -1,5 +1,25 @@
+import { ellipsis, padRight, prettyStack, StringDictionary } from '@skintest/common';
+import { AssertHost, DOMElement, ElementRef, ElementState, InspectInfo, TestFail } from '@skintest/sdk';
 import * as chalk from 'chalk';
+import * as path from 'path';
 import { WriteStream } from 'tty';
+
+const KNOWN_ERRORS = new Set(['skintest.timeout']);
+
+const STACK_FUNC_IGNORE = [
+  '__awaiter',
+  'fulfilled',
+  'rejected',
+];
+
+const STACK_FILE_IGNORE = [
+  path.join('platform', 'dist', 'src', 'attempt.js'),
+  path.join('platform', 'src', 'attempt.ts'),
+  path.join('common', 'src', 'errors.ts'),
+  // from playwright
+  // todo: propagate it here for the launcher options
+  path.join('lib', 'utils', 'stackTrace.js')
+];
 
 export const tty = {
   NEW_LINE: '\n',
@@ -30,4 +50,101 @@ export const tty = {
     const line = text.join('');
     stream.write(tty.NEW_LINE + line);
   },
+
+  writeFail(stream: WriteStream, reason: TestFail): void {
+    const { body } = reason;
+
+    // todo: make it better
+    const pivot = ['query', 'assert', 'actual', 'etalon'];
+    const isBinaryAssert = pivot.every(key => key in body);
+
+    if (isBinaryAssert) {
+      const selector = body.query.toString();
+      const method = body.query.type === 'query' ? '$' : '$$';
+      const assert: AssertHost = body.assert;
+
+      tty.newLine(stream, tty.fail(
+        `${method}(${selector}).${assert.what}: ` +
+        `expected ${tty.value('`' + body.actual + '`')} ` +
+        `to ${assert.no ? 'not' : ''} ${assert.how} ` +
+        `${tty.value('`' + body.etalon + '`')}`
+      ));
+    } else {
+      tty.newLine(stream, tty.fail(reason.description));
+    }
+  },
+
+  async writeError(stream: WriteStream, reason: Error): Promise<void> {
+    const message = (reason.name || 'Error') + ': ' + (reason.message || 'unknown error');
+    tty.newLine(stream, tty.error(padRight(message, stream.columns)));
+
+    if (!KNOWN_ERRORS.has(reason.name) && reason.stack) {
+      const stackTrace = await prettyStack(reason.stack);
+
+      stackTrace
+        .filter(x => x.file)
+        .filter(x => !STACK_FUNC_IGNORE.some(func => x.function === func))
+        .filter(x => !STACK_FILE_IGNORE.some(file => x.file.includes(file)))
+        .map(x => `${x.function || 'at'} (${x.file}:${x.line}:${x.column})`)
+        .forEach(x => tty.newLine(stream, tty.fail(x)));
+    }
+  },
+
+  async writeInspect(stream: WriteStream, inspect: InspectInfo): Promise<void> {
+    // eslint-disable-next-line prefer-const
+    let { selector, target } = inspect;
+    const maxWidth = 40;
+
+    target = Array.isArray(target)
+      ? target.length > 1
+        ? target
+        : target[0]
+      : target;
+
+    if (!target) {
+      tty.newLine(stream, tty.error(`$(\`${selector}\`) didn't find any elements`));
+      return;
+    }
+
+    if (Array.isArray(target)) {
+      tty.newLine(stream, `$(\`${selector}\`) found ${target.length} elements`);
+
+      const list: Array<StringDictionary<unknown>> = [];
+      for (const element of target) {
+        list.push({
+          text: ellipsis(await element.text(), maxWidth),
+          visible: await element.state('visible'),
+          enabled: await element.state('enabled'),
+        });
+      }
+
+      tty.newLine(stream);
+      console.table(list);
+    } else {
+      const elementRef = target as ElementRef<DOMElement>;
+      tty.newLine(stream, `$(\`${selector}\`) found 1 element`);
+
+      const info: StringDictionary<unknown> = {
+        text: ellipsis(await elementRef.text(), maxWidth),
+        classList: ellipsis((await elementRef.classList()).toString(), maxWidth),
+      };
+
+      const addState = async (key: ElementState) => {
+        try {
+          info[key] = await elementRef.state(key);
+          // eslint-disable-next-line no-empty
+        } catch (ex) {
+        }
+      };
+
+      await addState('checked');
+      await addState('editable');
+      await addState('enabled');
+      await addState('focused');
+      await addState('visible');
+
+      tty.newLine(stream);
+      console.table(info);
+    }
+  }
 };
